6b840938287a28c3a8344c1f0e2e6782fef19dcf
sagorkhan2012 <sagor2012.sk@gmail.com>
2026-05-15 06:12:26 +0600

    Added password length validation

--- views/auth/register.php
+++ views/auth/register.php
@@ -16,7 +16,12 @@
     <input type="email" name="email"><br><br>
 
     <label>Password</label><br>
-    <input type="password" name="password"><br><br>
+    <input
+type="password"
+name="password"
+minlength="8"
+required
+><br><br>
 
     <label>Select Role</label><br>
 
