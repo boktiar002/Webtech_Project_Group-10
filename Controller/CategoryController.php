ac5fc9c7c1a2d206d6fb6f5a00e7c75664472f0f
abir299 <mdabirahmedparves@gmail.com>
2026-05-16 13:20:46 +0600

    Add validation and comments to CategoryController

--- Controller/CategoryController.php
+++ Controller/CategoryController.php
@@ -4,21 +4,26 @@ require_once __DIR__ . "/../Model/Catagory.php";
 class CategoryController {
     public $category;
 
-    public function __construct(){
+    public function __construct() {
         $this->category = new Category();
     }
 
-    public function index(){
+    // Get all categories
+    public function index() {
         return $this->category->getAll();
     }
 
-    public function store($name){
+    // Create new category
+    public function store($name) {
+        $name = trim($name);
+        if(empty($name)) return false;
         $this->category->create($name);
         header("Location: /Webtech_Project_Group-10/index.php?page=categories");
         exit();
     }
 
-    public function delete($id){
+    // Delete category
+    public function delete($id) {
         return $this->category->delete($id);
     }
 }
